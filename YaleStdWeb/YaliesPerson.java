318c84265eed49c7395d0b02dd43e1f78bba9556
Eric Yoon <emjyoon@gmail.com>
2025-04-11 17:51:29 -0400

    WIP

---
+++ YaleStdWeb/YaliesPerson.java
@@ -0,0 +1,11 @@
+package YaleStdWeb;
+
+public class YaliesPerson {
+    public String netid;
+    public String email;
+    public String firstName;
+    public String lastName;
+    public String school;
+    public int year;
+    public String college;
+}
